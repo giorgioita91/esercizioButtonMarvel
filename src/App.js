c4c7d6f43e5f85aa0f5b3ac1992f4fa486d610d2
Andrea Biviano <biv@MacBook-Pro-di-Biv-2.local>
2020-11-05 11:30:05 +0100

    primo commit con un sacco di errori

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  var array = ['nome1';'nome2','nome3';'nome4'];
+      const : [
+        "HTML",
+        "css";
+        js Es5",
+        "js Es6,
+        "React"
+    ]
+    const heroes = [
+    {
+        id:1,
+        nome:"Tony",
+        cognome:"Stark",
+        eta:"40",
+        skill:"React"
+    },
+    {
+        id:2,
+        nome:"Steve",
+        cognome:"Rogers",
+        eta:"102",
+        skill:"Angular"
+    },
+    {
+        id:3,
+        nome:"Bruce",
+        cognome:"Banner",
+        eta:"39",
+        skill:"Vue"
+    },
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      ReactDOM.render(
+          React.createElement("ul",{id:'lista'},
+                      items.map( (item,index) => React.createElement('li',{index},item))),
+          document.getElementById('root')
+      )
+
+  const {id:x,nome:y,cognome:z,eta:v,:skill:k} = heroes;
+  function Hero(props){
+      <h2>Io {props.zeno.x} {props.zero.z} ho {props.zero.eta} anni, conosco molto bene {props.zero.k}</h2>
+  
+  }
+    
+    
+    
+    function App(){
+        const heroList = heroes.maps( (hero) => <Hero zeno={hero} />)
+        return(
+            heroList
+        )
+    }
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+ReactDOM.appendChild(
   <React.StrictMode>
-    <App />
+   <App/> {/* questo è il nostro componente radice chiamato APP */}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementsById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
